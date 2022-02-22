957acfc56a0f935e5eecef439a585e2a1a8e575a
Ítalo Santos <italotayllor@gmail.com>
2022-02-21 23:25:23 -0300

    feat: Adicionar método startAsyncTask

--- app/src/main/java/com/example/threadsconsumptionserviceapplication/AssincronaActivity.java
+++ app/src/main/java/com/example/threadsconsumptionserviceapplication/AssincronaActivity.java
@@ -3,12 +3,22 @@ package com.example.threadsconsumptionserviceapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class AssincronaActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assincrona);
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    private void startAsyncTask(View view) {
+
     }
 }
